a028046a5089142bb3ec0b0c1f4b025364fa0cf3
Alexander Aagaard <aa@signifly.com>
2019-10-04 09:14:43 +0200

    tabs init

--- src/components/tabs/index.vue
+++ src/components/tabs/index.vue
@@ -1,7 +1,60 @@
 <template>
-	<div>TABS</div>
+  <div class="tabs">
+    <router-link
+      class="tab"
+      v-for="item in tabs"
+      :key="item.name"
+      :to="item.path"
+      v-html="item.name"
+    />
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data: () => ({
+    tabs: [
+      {
+        name: "1",
+        path: "/path1"
+      },
+      {
+        name: "2",
+        path: "/path2"
+      },
+      {
+        name: "3",
+        path: "/path3"
+      },
+      {
+        name: "4",
+        path: "/path4"
+      },
+      {
+        name: "5",
+        path: "/path5"
+      }
+    ]
+  })
+};
 </script>
+
+<style lang="scss" scoped>
+.tabs {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: $tabs-height;
+  display: flex;
+  background-color: $black2;
+  .tab {
+    color: $white;
+    width: percentage(1/5);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-decoration: none;
+  }
+}
+</style>
